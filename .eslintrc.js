6356bb04275340a81edf082f12c4286180cfb291
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2024-12-26 10:53:12 -0500

    use electron, starting to add react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 const defaultLint = require('dzuelu-eslint-config');
 
-defaultLint.rules['node/no-missing-import'] = ["off"];
-defaultLint.rules['import/no-extraneous-dependencies'] = ["error", {"devDependencies": ["**/*.test.tsx"]}];
+defaultLint.rules['import/no-extraneous-dependencies'] = ['error', { devDependencies: true }];
+defaultLint.rules['node/no-unpublished-require'] = 'off';
+defaultLint.rules['node/no-extraneous-import'] = ['error', { "allowModules": ['webpack', '@electron-forge/shared-types'] }];
 
 module.exports = defaultLint;
