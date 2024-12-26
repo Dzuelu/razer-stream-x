f87d33afa6c485580706bb7a4f2ce4041601cf7a
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2024-12-26 14:56:42 -0500

    Add tray, fix lint, rename files to be more consistant

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 const defaultLint = require('dzuelu-eslint-config');
 
-// defaultLint.rules['import/no-extraneous-dependencies'] = ['error', { tryExtensions: [".ts", ".tsx"] }];
-defaultLint.rules['node/no-missing-import'] = ['error', { devDependencies: true }];
+defaultLint.rules['import/no-extraneous-dependencies'] = ['error', { devDependencies: true }];
+defaultLint.rules['node/no-missing-import'] = ['error', { tryExtensions: ['.ts', '.tsx', '.d.ts', '.js'] }];
 defaultLint.rules['node/no-unpublished-require'] = 'off';
-defaultLint.rules['node/no-extraneous-import'] = ['error', { "allowModules": ['webpack', '@electron-forge/shared-types'] }];
+defaultLint.rules['no-use-before-define'] = 'off';
+defaultLint.rules['node/no-extraneous-import'] = [
+  'error',
+  { allowModules: ['webpack', '@electron-forge/shared-types'] }
+];
 
 module.exports = defaultLint;
